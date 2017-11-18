9f98fa57f68e7606770f3633baa4bc76b1030d51
Dürauer Markus <markus.duerauer@feuerwehr.gv.at>
2017-11-18 12:38:10 +0100

    Initial commit

---
+++ src/main/java/at/ac/fhstp/is161505/scanner/Application.java
@@ -0,0 +1,40 @@
+package at.ac.fhstp.is161505.scanner;
+
+/**
+ * This file is part of rtl-sdr-scanner.
+ * <p>
+ * rtl-sdr-scanner is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * rtl-sdr-scanner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * Diese Datei ist Teil von rtl-sdr-scanner.
+ * <p>
+ * rtl-sdr-scanner ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ * <p>
+ * rtl-sdr-scanner wird in der Hoffnung, dass es nützlich sein wird, aber
+ * OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License für weitere Details.
+ * <p>
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ * <p>
+ * Created by n17405180 on 18.11.17.
+ */
+public class Application {
+
+
+
+}
